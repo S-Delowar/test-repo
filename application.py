9c942727017fd950a19f359dbea7951fa5409870
SAYED DELOWAR <61371586+S-Delowar@users.noreply.github.com>
2024-03-02 20:43:12 +0600

    Update application.py

--- application.py
+++ application.py
@@ -10,5 +10,9 @@ app=application
 def index():
     return render_template('index.html') 
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 if __name__=="__main__":
     app.run()  
